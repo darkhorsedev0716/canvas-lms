darkhorsedev0716/canvas-lms

commit 71a64419a058749d3880c8e4b48ab383681da41d

lock provisional graders from speedgrader when marks are made
test plan:
* enable moderated grading for a course and assignment
* have some students with submissions
* as a provisional grader (e.g. TA or teacher)
 should not be able to add a provisional mark for a student
 if someone else has already made one

(it should hide the grading sidebar and add a message)

* once students can be selected for moderation, it should
 let graders make one more mark

closes #CNVS-22018

Change-Id: I728380b169fdf40663f8216dc2723d83f5523eb0
Reviewed-on: https://gerrit.instructure.com/63441
Tested-by: Jenkins
Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
Product-Review: James Williams  <jamesw@instructure.com>

public/javascripts/speed_grader.js
@@ -77,6 +77,7 @@ define([
       assignmentUrl = $("#assignment_url").attr('href'),
       $full_height = $(".full_height"),
       $rightside_inner = $("#rightside_inner"),
+      $not_gradeable_message = $("#not_gradeable_message"),
       $comments = $("#comments"),
       $comment_blank = $("#comment_blank").removeAttr('id').detach(),
       $comment_attachment_blank = $("#comment_attachment_blank").removeAttr('id').detach(),
@@ -164,6 +165,8 @@ define([
       this.submission = $.grep(jsonData.submissions, function(submission, i){
         return submission.user_id === student.id;
       })[0];
+
+      this.submission_state = submissionState(this);
       jsonData.studentMap[student.id] = student;
     });
 
@@ -215,18 +218,26 @@ define([
         "not_graded": 1,
         "resubmitted": 2,
         "not_submitted": 3,
-        "graded": 4
+        "graded": 4,
+        "not_gradeable": 5
       };
       jsonData.studentsWithSubmissions.sort(compareBy(function(student){
+        debugger;
         return student &&
-          states[submissionStateName(student.submission)];
+          states[submissionState(student)];
       }));
     }
   }
 
-  function submissionStateName(submission) {
+  function submissionState(student) {
+    var submission = student.submission;
     if (submission && submission.workflow_state != 'unsubmitted' && (submission.submitted_at || !(typeof submission.grade == 'undefined'))) {
-      if (!submission.excused && (typeof submission.grade == 'undefined' || submission.grade === null || submission.workflow_state == 'pending_review')) {
+      // TODO: change this so it doesn't lock moderators out but instead changes it to pass in as final marks
+      if ((ENV.grading_role == 'provisional_grader' || ENV.grading_role == 'moderator')
+        && !student.needs_provisional_grade && submission.provisional_grade_id === null) {
+        // if we are a provisional grader and it doesn't need a grade (and we haven't given one already) then we shouldn't be able to grade it
+        return "not_gradeable";
+      } else if (!submission.excused && (typeof submission.grade == 'undefined' || submission.grade === null || submission.workflow_state == 'pending_review')) {
         return "not_graded";
       } else if (submission.grade_matches_current_submission) {
         return "graded";
@@ -238,12 +249,14 @@ define([
     }
   }
 
-  function formattedSubmissionStateName(raw, submission) {
+  function formattedsubmissionState(raw, submission) {
     switch(raw) {
       case "graded":
         return I18n.t('graded', "graded");
       case "not_graded":
         return I18n.t('not_graded', "not graded");
+      case "not_gradeable":
+        return I18n.t('graded', "graded");
       case "not_submitted":
         return I18n.t('not_submitted', 'not submitted');
       case "resubmitted":
@@ -252,9 +265,8 @@ define([
   }
 
   function classNameBasedOnStudent(student){
-    var raw = submissionStateName(student.submission);
-    var formatted = formattedSubmissionStateName(raw, student.submission);
-    return {raw: raw, formatted: formatted};
+    var formatted = formattedsubmissionState(student.submission_state, student.submission);
+    return {raw: student.submission_state, formatted: formatted};
   }
 
   // xsslint safeString.identifier MENU_PARTS_DELIMITER
@@ -1065,6 +1077,13 @@ define([
       }));
 
       $rightside_inner.scrollTo(0);
+      if (this.currentStudent.submission_state == 'not_gradeable') {
+        $rightside_inner.hide();
+        $not_gradeable_message.show();
+      } else {
+        $not_gradeable_message.hide();
+        $rightside_inner.show();
+      }
       this.showGrade();
       this.showDiscussion();
       this.showRubric();
@@ -1678,7 +1697,7 @@ define([
         $statusIcon = $status.find(".speedgrader-selectmenu-icon");
         $queryIcon = $query.find(".speedgrader-selectmenu-icon");
 
-        if(className.raw == "graded" && this == EG.currentStudent){
+        if(this == EG.currentStudent && (className.raw == "graded" || className.raw == "not_gradeable")){
           var studentInfo = EG.getStudentNameAndGrade()
           $queryIcon.text("").append("<i class='icon-check'></i>");
           $status.addClass("graded");

app/controllers/gradebooks_controller.rb
@@ -379,7 +379,7 @@ class GradebooksController < ApplicationController
           if submission[:provisional]
             is_final = submission[:final] && @context.grants_right?(@current_user, :moderate_grades)
             subs.each do |sub|
-              sub.apply_provisional_grade_filter!(sub.provisional_grade(@current_user, is_final))
+              sub.apply_provisional_grade_filter!(sub.provisional_grade(@current_user, final: is_final))
             end
           end
           @submissions += subs

app/controllers/submissions_controller.rb
@@ -625,7 +625,7 @@ class SubmissionsController < ApplicationController
           is_final = provisional && params[:submission][:final] && @context.grants_right?(@current_user, :moderate_grades)
           @submissions.each do |s|
             s.limit_comments(@current_user, session) unless @submission.grants_right?(@current_user, session, :submit)
-            s.apply_provisional_grade_filter!(s.provisional_grade(@current_user, is_final)) if provisional
+            s.apply_provisional_grade_filter!(s.provisional_grade(@current_user, final: is_final)) if provisional
           end
 
           flash[:notice] = t('assignment_submitted', 'Assignment submitted.')

app/models/assignment.rb
@@ -206,6 +206,8 @@ class Assignment < ActiveRecord::Base
     integration_data
     integration_id
     only_visible_to_overrides
+    moderated_grading
+    grades_published_at
   )
 
   def external_tool?
@@ -1326,6 +1328,20 @@ class Assignment < ActiveRecord::Base
     !moderated_grading? || grades_published_at.present?
   end
 
+  def student_needs_provisional_grade?(student, preloaded_counts=nil)
+    pg_count = if preloaded_counts
+      preloaded_counts[student.id.to_s] || 0
+    else
+      self.provisional_grades.not_final.where(:submissions => {:user_id => student}).count
+    end
+    in_moderation_set = if self.moderated_grading_selections.loaded?
+      self.moderated_grading_selections.detect{|s| s.student_id == student.id}.present?
+    else
+      self.moderated_grading_selections.where(:student_id => student).exists?
+    end
+    pg_count < (in_moderation_set ? 2 : 1)
+  end
+
   def speed_grader_json(user, avatars: false, grading_role: :grader, crocodoc_ids: nil)
     Attachment.skip_thumbnails = true
     submission_fields = [:user_id, :id, :submitted_at, :workflow_state,
@@ -1368,10 +1384,19 @@ class Assignment < ActiveRecord::Base
     all_provisional_rubric_assmnts = grading_role == :moderator &&
       (visible_rubric_assessments_for(user, :provisional_moderator => true) || [])
 
+    # if we're a provisional grader, calculate whether the student needs a grade
+    preloaded_pg_counts = is_provisional && self.provisional_grades.not_final.group("submissions.user_id").count
+    ActiveRecord::Associations::Preloader.new(self, :moderated_grading_selections).run if is_provisional # preload the association now
+
     res[:context][:students] = students.map do |u|
       json = u.as_json(:include_root => false,
                 :methods => avatar_methods,
                 :only => [:name, :id])
+
+      if preloaded_pg_counts
+        json[:needs_provisional_grade] = student_needs_provisional_grade?(u, preloaded_pg_counts)
+      end
+
       json[:rubric_assessments] = rubric_assmnts.select{|ra| ra.user_id == u.id}.
         as_json(:methods => [:assessor_name], :include_root => false)
 
@@ -1392,6 +1417,15 @@ class Assignment < ActiveRecord::Base
     includes << (grading_role == :grader ? :submission_comments : :all_submission_comments)
     submissions = self.submissions.where(:user_id => students).preload(*includes)
 
+    preloaded_prov_grades = case grading_role
+    when :moderator
+      self.provisional_grades.order(:id).to_a.group_by(&:submission_id)
+    when :provisional_grader
+      self.provisional_grades.where(:scorer_id => user).order(:id).to_a.group_by(&:submission_id)
+    else
+      {}
+    end
+
     res[:too_many_quiz_submissions] = too_many = too_many_qs_versions?(submissions)
     qs_versions = quiz_submission_versions(submissions, too_many)
 
@@ -1404,7 +1438,7 @@ class Assignment < ActiveRecord::Base
       ).merge("from_enrollment_type" => enrollment_types_by_id[sub.user_id])
 
       if grading_role == :provisional_grader || grading_role == :moderator
-        provisional_grade = sub.provisional_grade(user)
+        provisional_grade = sub.provisional_grade(user, preloaded_grades: preloaded_prov_grades)
         json.merge! provisional_grade.grade_attributes
       end
 
@@ -1450,7 +1484,7 @@ class Assignment < ActiveRecord::Base
                                    end
 
       if grading_role == :moderator
-        json['provisional_grades'] = sub.provisional_grades.order(:id).map do |pg|
+        json['provisional_grades'] = (preloaded_prov_grades[sub.id] || []).map do |pg|
           pg.grade_attributes.tap do |json|
             json[:submission_comments] = pg.submission_comments.as_json(:include_root => false,
                                                                         :methods => avatar_methods,

app/models/moderated_grading/selection.rb
@@ -4,4 +4,6 @@ class ModeratedGrading::Selection < ActiveRecord::Base
     class_name: 'ModeratedGrading::ProvisionalGrade'
   belongs_to :assignment
   belongs_to :student, class_name: 'User'
+
+  attr_accessible :student
 end

app/models/submission.rb
@@ -917,11 +917,20 @@ class Submission < ActiveRecord::Base
     false
   end
 
-  def provisional_grade(scorer, final=false)
-    pg = if final
-      self.provisional_grades.final.first
+  def provisional_grade(scorer, final: false, preloaded_grades: nil)
+    pg = if preloaded_grades
+      pgs = preloaded_grades[self.id] || []
+      if final
+        pgs.detect{|pg| pg.final}
+      else
+        pgs.detect{|pg| !pg.final && pg.scorer_id == scorer.id}
+      end
     else
-      self.provisional_grades.not_final.where(scorer_id: scorer).first
+      if final
+        self.provisional_grades.final.first
+      else
+        self.provisional_grades.not_final.where(scorer_id: scorer).first
+      end
     end
     pg ||= ModeratedGrading::NullProvisionalGrade.new(scorer.id, final)
     pg
@@ -931,6 +940,9 @@ class Submission < ActiveRecord::Base
     ModeratedGrading::ProvisionalGrade.unique_constraint_retry do
       pg = final ? self.provisional_grades.final.first : self.provisional_grades.not_final.where(scorer_id: scorer).first
       unless pg
+        unless final || self.assignment.student_needs_provisional_grade?(self.user)
+          raise Assignment::GradeError.new("Student already has the maximum number of provisional grades")
+        end
         pg = self.provisional_grades.build
       end
       pg.scorer_id = scorer.id

app/stylesheets/bundles/speed_grader.scss
@@ -307,6 +307,11 @@ $gradebook_header_height: 52px;
     @extend %flex-1;
     overflow-y: auto;
   }
+  #not_gradeable_message {
+    padding: 20px;
+    font-size: 1.2em;
+    display: none;
+  }
 }
 
 .right_side_content {
@@ -548,6 +553,10 @@ body .ui-selectmenu-dropdown:hover .ui-selectmenu-icon, body .ui-state-active .u
   .resubmitted & {
     font-size: 1.7em;
     color: $orange;
+  }
+  .not_gradeable & {
+    font-size: 1.7em;
+    color: $grayLight;
   }
   .graded & {
     color: $green;

app/views/gradebooks/speed_grader.html.erb
@@ -94,6 +94,7 @@
     </div>
   </div>
   <div id="right_side" class="full_height">
+    <div id="not_gradeable_message"><%= t("This submission has already been graded and is ready for moderation.") %></div>
     <div class="full_height" id="width_resizer"></div>
     <div class=right_side_content>
       <div id="rightside_inner" class="full_height">

commit 561a73acc293db3e8914c882c37f4bbdfcebdb74

graded icon updates when SpeedGrader's scores are updated
fixes CNVS-23976

This regression was introduced by 71a64419.

test plan:
 0. As a teacher, create a quiz with an essay question.
 1. As a student, take the quiz.
 2. As the teacher, go to the SpeedGrader.
 3. Enter a score for the essay quiz question.
 4. Click the "Update Scores" button.
 5. Verify the orange dot next to the students name becomes
    a green check mark.
 6. Delete the score for the essay quiz question.
 7. Click the "Update Scores" button.
 8. Verify the green check mark next to the students name
    becomes an orange dot.

Change-Id: I2b7b1ce787375f60fb3c8967b520049108bf5edf
Reviewed-on: https://gerrit.instructure.com/67016
Tested-by: Jenkins
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
QA-Review: Jason Carter <jcarter@instructure.com>
Product-Review: Keith T. Garner <kgarner@instructure.com>

public/javascripts/speed_grader.js
@@ -291,8 +291,9 @@ define([
   }
 
   function classNameBasedOnStudent(student){
+    var raw       = submissionState(student);
     var formatted = formattedsubmissionState(student.submission_state, student.submission);
-    return {raw: student.submission_state, formatted: formatted};
+    return {raw: raw, formatted: formatted};
   }
 
   // xsslint safeString.identifier MENU_PARTS_DELIMITER