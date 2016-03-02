darkhorsedev0716/canvas-lms

commit 696f099e7c0e21b11b59542284177002fadf1e7f

sort assignments in gradebook by teacher settings when MGP is on
fixes CNVS-25686

Test plan:
 0. With multiple grading periods enabled in a course with
    many periods, a teacher, and a student…
 1. As the teacher, create many assignments in several
    different grading periods, ensuring the assignments are
    not created in calendar order.
 2. As the teacher select "Arrange columns by due date" in
    gradebook settings.
 3. Refresh the page and verify that the assignments are
    sorted by due date.
 4. As the teacher select "Arrange columns by assignment
    group" in gradebook settings.
 5. Refresh the page and verify that the assignments are
    not sorted by due date.

Change-Id: Iaa590e29d9bf512c0b2ac12dad2b7c64a2b579ab
Reviewed-on: https://gerrit.instructure.com/69253
Reviewed-by: Derek Bender <djbender@instructure.com>
QA-Review: Amber Taniuchi <amber@instructure.com>
Tested-by: Jenkins
Product-Review: Strand McCutchen <smccutchen@instructure.com>

app/coffeescripts/gradebook2/Gradebook.coffee
@@ -1272,14 +1272,21 @@ define [
       @buildRows()
 
     getVisibleGradeGridColumns: ->
-      res = [].concat @parentColumns, @customColumnDefinitions()
+      columns = []
+
       for column in @allAssignmentColumns
         if @disabledAssignments && @disabledAssignments.indexOf(column.object.id) != -1
           column.cssClass = "cannot_edit"
         submissionType = ''+ column.object.submission_types
-        res.push(column) unless submissionType is "not_graded" or
-                                submissionType is "attendance" and !@show_attendance
-      res.concat(@aggregateColumns)
+        columns.push(column) unless submissionType is "not_graded" or
+                                submissionType is "attendance" and not @show_attendance
+
+      if @gradebookColumnOrderSettings?.sortType
+        columns.sort @makeColumnSortFn(@gradebookColumnOrderSettings)
+
+      columns = columns.concat(@aggregateColumns)
+      headers = @parentColumns.concat(@customColumnDefinitions())
+      headers.concat(columns)
 
     assignmentHeaderHtml: (assignment) ->
       columnHeaderTemplate

spec/coffeescripts/gradebook2/GradebookSpec.coffee
@@ -516,3 +516,31 @@ define [
         all_grading_periods_totals: true
 
     notOk @hideAggregateColumns.call(self)
+
+  module 'Gradebook#getVisibleGradeGridColumns',
+    setup: ->
+      @getVisibleGradeGridColumns = Gradebook.prototype.getVisibleGradeGridColumns
+      @makeColumnSortFn = Gradebook.prototype.makeColumnSortFn
+      @compareAssignmentPositions = Gradebook.prototype.compareAssignmentPositions
+      @wrapColumnSortFn = Gradebook.prototype.wrapColumnSortFn
+      @allAssignmentColumns = [
+          { object: { assignment_group: { position: 1 }, position: 1, name: "first" } },
+          { object: { assignment_group: { position: 1 }, position: 2, name: "second" } },
+          { object: { assignment_group: { position: 1 }, position: 3, name: "third" } }
+        ]
+      @aggregateColumns = []
+      @parentColumns = []
+      @customColumnDefinitions = -> []
+      spy = @spy(this, 'makeColumnSortFn')
+    teardown: ->
+
+  test 'It sorts columns when there is a sortType', ->
+    @gradebookColumnOrderSettings = { sortType: 'assignment_group' }
+    @getVisibleGradeGridColumns()
+    ok @makeColumnSortFn.calledOnce
+
+
+  test 'It does not sort columns when gradebookColumnOrderSettings is undefined', ->
+    @gradebookColumnOrderSettings = undefined
+    @getVisibleGradeGridColumns()
+    notOk @makeColumnSortFn.called

commit 1e3e43839c974ffc202ffec61d9eb2b6f6136cb2

use default sort settings if custom sort is invalid
in the gradebook, we should default to sorting by
assignment group if the custom sort setting is
invalid. we normally do default to sorting by
assignment group if the custom sort setting is
invalid, but a regression was introduced in
696f099e7c0e21b11b59542284177002fadf1e7f

closes CNVS-27545

test plan:
- enter a rails console and find a teacher for
  one of your courses
  $ bundle exec rails console
  $ course = Course.find(1)
  $ teacher = course.teachers.active.first

- set the teacher's gradebook sort settings to
  an invalid custom sort (it is invalid if it
  does not have a customOrder property)
  $ teacher.preferences[:gradebook_column_order] =
    { course.id => { "sortType" => "custom" } }
  $ teacher.save!

- log in as the teacher above and go to the
  gradebook for the course above

- verify the gradebook loads

Change-Id: I84a4d620c2cc1c4fe38d383cd0db254e30e9d245
Reviewed-on: https://gerrit.instructure.com/73448
Reviewed-by: Derek Bender <djbender@instructure.com>
Tested-by: Jenkins
QA-Review: KC Naegle <knaegle@instructure.com>
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
Product-Review: Keith T. Garner <kgarner@instructure.com>

app/coffeescripts/gradebook2/Gradebook.coffee
@@ -1289,7 +1289,7 @@ define [
                                 submissionType is "attendance" and not @show_attendance
 
       if @gradebookColumnOrderSettings?.sortType
-        columns.sort @makeColumnSortFn(@gradebookColumnOrderSettings)
+        columns.sort @makeColumnSortFn(@getStoredSortOrder())
 
       columns = columns.concat(@aggregateColumns)
       headers = @parentColumns.concat(@customColumnDefinitions())

spec/coffeescripts/gradebook2/GradebookSpec.coffee
@@ -601,7 +601,10 @@ define [
       @getVisibleGradeGridColumns = Gradebook.prototype.getVisibleGradeGridColumns
       @makeColumnSortFn = Gradebook.prototype.makeColumnSortFn
       @compareAssignmentPositions = Gradebook.prototype.compareAssignmentPositions
+      @compareAssignmentDueDates = Gradebook.prototype.compareAssignmentDueDates
       @wrapColumnSortFn = Gradebook.prototype.wrapColumnSortFn
+      @getStoredSortOrder = Gradebook.prototype.getStoredSortOrder
+      @defaultSortType = 'assignment_group'
       @allAssignmentColumns = [
           { object: { assignment_group: { position: 1 }, position: 1, name: "first" } },
           { object: { assignment_group: { position: 1 }, position: 2, name: "second" } },
@@ -610,14 +613,22 @@ define [
       @aggregateColumns = []
       @parentColumns = []
       @customColumnDefinitions = -> []
-      spy = @spy(this, 'makeColumnSortFn')
+      @spy(this, 'makeColumnSortFn')
     teardown: ->
 
-  test 'It sorts columns when there is a sortType', ->
-    @gradebookColumnOrderSettings = { sortType: 'assignment_group' }
+  test 'It sorts columns when there is a valid sortType', ->
+    @isInvalidCustomSort = -> false
+    @columnOrderHasNotBeenSaved = -> false
+    @gradebookColumnOrderSettings = { sortType: 'due_date' }
     @getVisibleGradeGridColumns()
-    ok @makeColumnSortFn.calledOnce
+    ok @makeColumnSortFn.calledWith { sortType: 'due_date' }
 
+  test 'It falls back to the default sort type if the custom sort type does not have a customOrder property', ->
+    @isInvalidCustomSort = -> true
+    @gradebookColumnOrderSettings = { sortType: 'custom' }
+    @makeCompareAssignmentCustomOrderFn = Gradebook.prototype.makeCompareAssignmentCustomOrderFn
+    @getVisibleGradeGridColumns()
+    ok @makeColumnSortFn.calledWith { sortType: 'assignment_group' }
 
   test 'It does not sort columns when gradebookColumnOrderSettings is undefined', ->
     @gradebookColumnOrderSettings = undefined