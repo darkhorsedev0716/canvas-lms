darkhorsedev0716/canvas-lms

commit 5d998e808edfd920479d025d04eed227aea080be

clean up old webpack artifacts after each build
closes: CNVS-36233

webpack adds a fingerprint to the filenames that it outputs, which 
means that if the content changes, the filename changes. so over 
time, unless you manually know to rm -rf public/dist/webpack* 
periodically, you'll start accumulating a ton of javascript files 
in that dir that you don't need which can slow down your editor's 
grepping, take up a ton of space, and confuse you. this change makes 
it so right before webpack emits all of the new bundles, it cleans up 
the ones that were there. it only cleans up the ones in the dir it is 
outputting to, so you can still have prod bundles in 
public/dist/webpack-production/ and dev bundles in 
public/dist/webpack-development

test plan:
* run webpack before you check this out
* check this out
* run yarn then run webpack again
* you should see something in the webpack output like:
  WebpackCleanupPlugin: 220 file(s) deleted.
* and there should be only one copy of each bundle in the 
  public/dist/webpack-development dir.

Change-Id: Ide7a7f53c255c7aa5ad09fb4ff0bcfd7436b2852
Reviewed-on: https://gerrit.instructure.com/108147
Tested-by: Jenkins
Reviewed-by: Clay Diffrient <cdiffrient@instructure.com>
Product-Review: Ryan Shaw <ryan@instructure.com>
QA-Review: Ryan Shaw <ryan@instructure.com>

frontend_build/baseWebpackConfig.js
@@ -12,6 +12,7 @@ const I18nPlugin = require('./i18nPlugin')
 const SelinimumManifestPlugin = require('./SelinimumManifestPlugin')
 const WebpackHooks = require('./webpackHooks')
 const webpackPublicPath = require('./webpackPublicPath')
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin')
 const HappyPack = require('happypack')
 require('babel-polyfill')
 
@@ -231,6 +232,8 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
+
+    new WebpackCleanupPlugin(),
 
     // handles our custom i18n stuff
     new I18nPlugin(),

commit 669e2dcdd67e0db804da6b3b859c89c927a19fd9

spec: don't nuke the selinimum manifest
regression in 5d998e808e

this should get build times back down again, as more commits will be
selinimizable

Change-Id: I202fe22b734e29afb6c0e536cafb7e6f81eac071
Reviewed-on: https://gerrit.instructure.com/109298
Tested-by: Jenkins
Reviewed-by: Landon Wilkins <lwilkins@instructure.com>
Product-Review: Landon Wilkins <lwilkins@instructure.com>
QA-Review: Landon Wilkins <lwilkins@instructure.com>

frontend_build/baseWebpackConfig.js
@@ -223,7 +223,9 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
     }),
 
-    new WebpackCleanupPlugin(),
+    new WebpackCleanupPlugin({
+      exclude: ["selinimum-manifest.json"]
+    }),
 
     // handles our custom i18n stuff
     new I18nPlugin(),